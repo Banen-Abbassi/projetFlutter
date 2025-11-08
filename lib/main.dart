af05e8a4e6787fdccd568f968b88bd4c5d019563
Banen-Abbassi <banenabbassi@gmail.com>
2025-11-08 21:32:25 +0100

    loading page + quit button

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,63 @@
 import 'package:chat_app/auth/auth_gate.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'themes/light_modes.dart'; // optional, if you have a theme
+import 'themes/light_modes.dart';
 import 'package:provider/provider.dart';
 import 'themes/theme_service.dart';
 import 'themes/dark_mode.dart';
+import 'pages/loading_page.dart'; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-runApp(
+
+  runApp(
     ChangeNotifierProvider(
       create: (_) => ThemeService(),
       child: const MyApp(),
     ),
   );
-  }
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    // Wait for 1 seconds before navigating to AuthGate
+    Future.delayed(const Duration(seconds: 1), () {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-      final themeService = Provider.of<ThemeService>(context);
+    final themeService = Provider.of<ThemeService>(context);
 
     return MaterialApp(
       title: 'Chat App',
       debugShowCheckedModeBanner: false,
       theme: lightMode,
       darkTheme: darkMode,
-      themeMode: themeService.isDarkMode ? ThemeMode.dark : ThemeMode.light,
-      home: const AuthGate(),
+      themeMode:
+          themeService.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+
+      // ✅ Display LoadingPage first, then AuthGate
+      home: _isLoading ? const LoadingPage() : const AuthGate(),
     );
   }
 }
--- lib/themes/dark_mode.dart
+++ lib/themes/dark_mode.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 final ThemeData darkMode = ThemeData(
   brightness: Brightness.dark,
   colorScheme: const ColorScheme.dark(
-    primary: Color(0xFFBB86FC),   // Light purple
-    secondary: Color(0xFF9C27B0), // Deep purple
+    primary: Color.fromARGB(255, 173, 63, 177),   // Light purple
+    secondary: Color.fromARGB(255, 115, 37, 129), // Deep purple
     tertiary: Colors.white,
     background: Color(0xFF121212), // Dark background
   ),
